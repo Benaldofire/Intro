d0c7c8ff86d428103c06139c149e4fc9d6786dc7
Benaldofire <Benedict.Odai@coyotes.usd.edu>
2021-03-26 03:43:34 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const spotify = document.getElementById("spotify");
+
+function spotifyFrame(){
+    if(spotify.style.display === "none"){
+        spotify.style.display = "block";
+    }
+    else{
+        spotify.style.display ="none";
+    }
+}
